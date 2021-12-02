54b906c87c076866da6eb3e0134e612004262a4c
Louis DECUP <posedeus@hotmail.fr>
2021-12-02 17:27:43 +0100

    Update of the Wiregrid parser to linear mode

--- wiregrid.py
+++ wiregrid.py
@@ -1,10 +1,8 @@
 #This module handles wire grids (see day 3)
 
-orY = 10000
-orX = 10000
 
 #Initialize a grid
-def iniGrid(size):
+def iniGrid(size,orX,orY):
 	grid = []
 	for i in range(size):
 		temp = []
@@ -28,7 +26,7 @@ def addChar(y,x,grid):
 	return grid
 
 #Parse a list of commands onto a grid
-def parseCom(input,grid):
+def parseCom(input,grid,orX,orY):
 	commands = input.split(",")
 	y = len(grid)-1-orY
 	x = orX
@@ -55,6 +53,36 @@ def parseCom(input,grid):
 				  
 	return grid
 
+#Parse a list of commands as positions
+def parseComLin(input):
+	output = [[0,0]]
+	commands = input.split(',')
+	for i in range(len(commands)):
+		pos = output[len(output)-1]
+		com = list(commands[i])[0]
+		val = int(commands[i][1:])
+		if com == "U":
+			for a in range(1,val+1):
+				pos = output[-1]
+				newpos = [pos[0],pos[1]+1]
+				output.append(newpos)
+		if com == "D":
+			for a in range(1,val+1):
+				pos = output[-1]
+				newpos = [pos[0],pos[1]-1]
+				output.append(newpos)
+		if com == "L":
+			for a in range(1,val+1):
+				pos = output[-1]
+				newpos = [pos[0]-1,pos[1]]
+				output.append(newpos)
+		if com == "R":
+			for a in range(1,val+1):
+				pos = output[-1]
+				newpos = [pos[0]+1,pos[1]]
+				output.append(newpos)
+	return output
+
 #Find crossroads
 def findX(grid):
 	values = []
@@ -65,7 +93,7 @@ def findX(grid):
 	return values
 
 #Find the distance between a point and the origin
-def cabDistance(y,x,size):
+def cabDistance(y,x,size,orX,orY):
 	distY = abs(size-1-orY - y)
 	distX = abs(orX - x)
 	return distX+distY
